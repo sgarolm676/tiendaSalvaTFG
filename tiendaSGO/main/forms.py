0ff8f136365b21667c6f5f19eee60ac155d0fd3b
sgarolm676 <sgarolm676@g.educaand.es>
2025-05-12 11:20:30 +0200

    prueba

--- tiendaSGO/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.utils.translation import gettext_lazy as _
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100, label=_("Usuario"))
-    password = forms.CharField(max_length=100, label=_("Contraseña"), widget=forms.PasswordInput)
-
--- tiendaSGO/main/test_views.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-
-class MainViewTests(TestCase):
-
-    def test_welcome(self):
-        response = self.client.get("/")
-        self.assertEquals(response.status_code, 200)

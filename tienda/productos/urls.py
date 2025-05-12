1a7d94122999abeb0106601afa5102a1e996f682
sgarolm676 <sgarolm676@g.educaand.es>
2025-05-12 11:16:03 +0200

    Error registro usuario correcto

--- tienda/productos/urls.py
+++ tienda/productos/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
 from . import views
-from .views import lista_productos, cerrar_sesion
+from .views import lista_productos, cerrar_sesion, registrar_usuario
 
 urlpatterns = [
     path('', lista_productos, name='lista_productos'),
-    path('register/', views.register, name='register'),
+    path('register/', registrar_usuario, name='register'),
     path('registration/login', include('django.contrib.auth.urls')),
     path('logout/', cerrar_sesion, name='logout'),
 ]

bd5359b93fc1f16b8ac5834badc98c979cdaac81
sgarolm676 <sgarolm676@g.educaand.es>
2025-06-03 20:40:41 +0200

    Estilos de Login y Logout

--- my_docker_django_app/my_docker_django_app/settings.py
+++ my_docker_django_app/my_docker_django_app/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'my_docker_django_app.urls'
 LOGIN_REDIRECT_URL = 'lista_productos' # Página después del login 
-LOGOUT_REDIRECT_URL = 'lista_productos' # Página después del logout  
+LOGOUT_REDIRECT_URL = 'login' # Página después del logout  
 
 STRIPE_PUBLISHABLE_KEY = 'pk_test_51RRXWpCdXtqJTGY6anR2xe3SE7MHGh2WmicU9CkRMrktj7uONwCkqyEvR7ORWjk2vkYhhDtQfgk2wfMr9WXUapFJ00bPUc020l'
 STRIPE_SECRET_KEY = 'sk_test_51RRXWpCdXtqJTGY6WQCuapaSSjQ7gSwwG7loFrV0d1zenSsyuOe2RLN8Ja64wfrmmycMpDHpT6YCAYn2hY0bCOSv00tanOCinX'
--- my_docker_django_app/productos/signals.py
+++ my_docker_django_app/productos/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Perfil, DireccionEnvio, TarjetaPago
+from .models import Perfil, DireccionEnvio, TarjetaPago, Carrito
 
 @receiver(post_save, sender=User)
 def crear_perfil_usuario(sender, instance, created, **kwargs):
@@ -17,3 +17,8 @@ def crear_perfil_configuracion(sender, instance, created, **kwargs):
     if created:
         DireccionEnvio.objects.create(usuario=instance)
         TarjetaPago.objects.create(usuario=instance)
+
+@receiver(post_save, sender=User)
+def crear_carrito_usuario(sender, instance, created, **kwargs):
+    if created:
+        Carrito.objects.create(usuario=instance)

54864577a251d9b784c86d4002951aad5b0a9ace
sgarolm676 <sgarolm676@g.educaand.es>
2025-06-06 17:41:15 +0200

    Subida de modelo Entidad Relación

--- tienda/productos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Producto, DireccionEnvio, TarjetaPago, Perfil
-
-# Register your models here.
-admin.site.register(Producto)
-admin.site.register(Perfil)
-admin.site.register(DireccionEnvio)
-admin.site.register(TarjetaPago)
--- tienda/productos/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class ProductosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'productos'
-
-    def ready(self):
-        import productos.signals  # importa señales al iniciar la app
